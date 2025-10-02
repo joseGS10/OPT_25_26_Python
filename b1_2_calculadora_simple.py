58cb9e7d9aa8205f77089b37d2d218b7d79b443f
José Burguillo <josua75@hotmail.es>
2025-10-02 21:16:56 +0200

    2do ejercicio

---
+++ b1_2_calculadora_simple.py
@@ -0,0 +1,9 @@
+num1 = int(input("Dame un número: "))
+num2 = int(input("Dame otro número: "))
+print(f"la suma de {num1} + {num2} es: {num1 + num2}")
+print(f"la resta de {num1} - {num2} es: {num1 - num2}")
+print(f"la multiplicación de {num1} * {num2} es: {num1 * num2}")
+print(f"la división de {num1} / {num2} es {num1 / num2}")
+print(f"El módulo de {num1} % {num2} es {num1 % num2}")
+print(f"La potencia {num1} ^ {num2} es: {num1 ** num2}")
+
