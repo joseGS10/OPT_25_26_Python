53bf9a3fc7b71904d52c966e1191db6add286709
José Burguillo <josua75@hotmail.es>
2025-10-03 00:21:56 +0200

    2do ejercicio

---
+++ b1_3_conversor_datos.py
@@ -0,0 +1,5 @@
+nombre = input("Deme su nombre: ")
+edad = int(input("Deme su edad: "))
+altura = float(input("Deme su altura: "))
+print(f"Hola, me llamo {nombre}, tengo {edad} años y mido {altura} metros.")
+
