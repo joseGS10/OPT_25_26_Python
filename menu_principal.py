706abfedaf0e46ae294b75a6d1b27748e3ba508f
José Burguillo <josua75@hotmail.es>
2025-10-29 19:50:24 +0100

    retos bloque02

---
+++ menu_principal.py
@@ -0,0 +1,12 @@
+def mostrar_menu():
+    """
+    Esta función muestra por pantalla un menú
+    """
+    print("1.Jugar.")
+    print("2.Configuración.")
+    print("3.Salir.")
+
+mostrar_menu()
+mostrar_menu()
+
+
