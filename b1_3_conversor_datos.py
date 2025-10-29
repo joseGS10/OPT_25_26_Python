cac3099137594440751c6459290ce812ed82c6d5
JoseBurguillo <josua75@hotmail.es>
2025-10-29 19:55:45 +0100

    Delete b1_3_conversor_datos.py

--- b1_3_conversor_datos.py
+++
@@ -1,5 +0,0 @@
-nombre = input("Deme su nombre: ")
-edad = int(input("Deme su edad: "))
-altura = float(input("Deme su altura: "))
-print(f"Hola, me llamo {nombre}, tengo {edad} años y mido {altura} metros.")
-
