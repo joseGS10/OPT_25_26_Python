06d6ca8cd97f55e6696e9196bfda9e742c7bee57
José Burguillo <josua75@hotmail.es>
2025-10-30 19:54:57 +0100

    retos bloque02

---
+++ dias_semana.py
@@ -0,0 +1,13 @@
+semana = ("lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo")
+
+print(f"El primer dia de la semana es {semana[0]}")
+print(f"El último dia de la semana es {semana[-1]}")
+
+for dia in semana:
+    print(dia)
+
+print(f"El día Miercoles se encuentra en la posición {semana.index("miercoles")}")
+
+
+
+
---
+++ local-global.py
@@ -0,0 +1,13 @@
+curso = "Python"
+
+def mostrar_curso():
+    print(f"Estamos dando un curso de {curso}")
+
+def cambiar_curso():
+    curso= "Java"
+    print(f"Estamos dentro del curso de {curso}")
+
+mostrar_curso()
+cambiar_curso()
+
+
