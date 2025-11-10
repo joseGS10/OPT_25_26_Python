59fc55aec1a71c8bcfe85380ad817837bebd46c8
José Burguillo <josua75@hotmail.es>
2025-11-10 09:37:51 +0100

    retos bloque02

--- diciionario_crud.py
+++ diciionario_crud.py
@@ -1,20 +1,18 @@
-#people = {"123456789": {"nombre": "David", "age": 14, "city": "Huelva", "profesion": "Estudiente"},
-#          "987654321": {"nombre": "Ana", "age": 28, "city": "Cadiz", "profesion": "Azafata"}}
+#people = {"222222222": {"dni":"222222222", "nombre": "David", "age": 14, "city": "Huelva", "profesion": "Estudiente"},
+#          "111111111": {"dni":"111111111", "nombre": "Ana", "age": 28, "city": "Cadiz", "profesion": "Azafata"}}
 
-people = {}  # Main dictionary: {nif: {name, age, city, profession}}
+people = {}  # Main dictionary: {nif: {dni, name, age, city, profession}}
 
 
-def create_person():
+def create_person(person):
     """Create a new person and add to the dictionary."""
     # TODO: Ask for ID, name, age, city, profession and add to people
-    nif = input("Introduzca su DNI: ")
-    name = input("Introduzca su Nombre: ")
-    age = int(input("Introduzca su edad: "))
-    city = input("Introduzca su Ciudad: ")
-    profession = input("Introduzca su profesion: ")
 
-    people[nif] = {"name" : name, "age" : age, "city" : city, "profession" : profession}
-
+    if person["dni"] in people:
+        print(f"La persona con dni {person["dni"]} ya existe. No se añadirá de nuevo")
+    else:
+        people[person["dni"]] = person
+        print("Persona creada correctamente.")
 
 def read_people():
     """Display all registered people."""
@@ -22,35 +20,35 @@ def read_people():
     for clave, valores in people.items():
         print(f"NIF: {clave} -> valores{valores}")
 
-def update_person():
+def update_person(dni_actualizar):
     """Update information of an existing person."""
     # TODO: Ask for ID, check if exists, and update fields
-    nif_actualizar = input("Introduce nif de la persona a actualizar: ")
-    if nif_actualizar in people:
+    # Si la persona a actualizar se encuentra registrada
+    if dni_actualizar in people:
         print("Actualizaión de los valores de los campos. Intro si no se desea modificar.")
-        for campo, valor in people[nif_actualizar].items():
-            nuevo_valor = input(f"{campo.capitalize()} actual ({valor}): ")
+        for clave, valor in people[dni_actualizar].items():
+            nuevo_valor = input(f"{clave} actual ({valor}): ")
             if nuevo_valor:
                 # Convertir edad a entero si se edita
-                if campo == "age":
-                    people[nif_actualizar][campo] = int(nuevo_valor)
+                if clave == "age":
+                    people[nif_actualizar][clave] = int(nuevo_valor)
                 else:
-                    people[nif_actualizar][campo] = nuevo_valor
-
+                    people[nif_actualizar][clave] = nuevo_valor
         print("Persona actualizada con éxito.")
-    #if nif_actualizar not in people:
-    else:
-        print(f"La persona de NIF {nif_actualizar} no existe aún")
 
-def delete_person():
+    # Si la persona a actualizar no se encuentra registrada
+    else:
+        print(f"La persona de NIF {dni_actualizar} no existe aún")
+
+def delete_person(dni_borrar):
     """Delete a person by ID."""
     # TODO: Ask for ID and remove from the dictionary if exists
-    nif_borrar = input("Intro nif de la persona a eliminar: ")
-    if (nif_borrar) in people:
+    # Si la persona a eliminar está registrada
+    if (dni_borrar) in people:
         del people[nif_borrar]
-        print(f"La persona con NIF {nif_borrar} ha sido eliminada.")
+        print(f"La persona con NIF {dni_borrar} ha sido eliminada.")
     else:
-        print(f"La persona con NIF {nif_borrar} no exite.")
+        print(f"La persona con NIF {dni_borrar} no exite.")
 
 
 # 🔸 Main menu
@@ -68,14 +66,26 @@ while option != "5":
 
     match option:
         case "1":
-            create_person()
-            print("Persona creada correctamente.")
+            persona = {}
+            nif = input("Introduzca su DNI: ")
+            name = input("Introduzca su Nombre: ")
+            age = int(input("Introduzca su edad: "))
+            city = input("Introduzca su Ciudad: ")
+            profession = input("Introduzca su profesion: ")
+            persona = {"dni":nif, "name":name, "age":age, "city":city, "profession":profession}
+            create_person(persona)
+
         case "2":
             read_people()
+
         case "3":
-            update_person()
+            nif_actualizar = input("Introduce nif de la persona a actualizar: ")
+            update_person(nif_actualizar)
+
         case "4":
-            delete_person()
+            nif_borrar = input("Intro nif de la persona a eliminar: ")
+            delete_person(nif_borrar)
+
         case "5":
             print("Exiting program...")
         case _:
